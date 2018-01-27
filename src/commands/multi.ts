956a6b9fd745459da07eda73f71d4057d91282ba
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-26 21:23:36 -0800

    fix: add other commands

---
+++ src/commands/multi.ts
@@ -0,0 +1,25 @@
+import Command, {flags} from '@dxcli/command'
+import {createEnv} from 'yeoman-environment'
+
+export default class extends Command {
+  static flags: flags.Input = {
+    defaults: flags.boolean({description: 'use defaults for every setting'}),
+    options: flags.string({description: '(typescript|semantic-release|mocha)'}),
+  }
+  static args = [
+    {name: 'path', required: false}
+  ]
+
+  async run() {
+    const env = createEnv()
+
+    env.register(
+      require.resolve('../generators/app'),
+      'dxcli:app'
+    )
+
+    const options = this.flags.options ? this.flags.options.split(',') : []
+
+    await env.run('dxcli:app', {type: 'multi', path: this.args.path, options, defaults: this.flags.defaults})
+  }
+}

3659d8652208e48b4bd5cf3bb9a67ee8f2c5aca6
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-28 06:09:54 -0800

    fix: fixing single and adding windows cmd scripts

--- templates/single/test/index.test.js
+++ templates/single/test/index.test.js
@@ -5,12 +5,12 @@ describe('command', () => {
   test
     .stdout()
     .do(() => cmd.run([]))
-    .do(output => expect(output.stdout).to.equal('hello world!\n'))
+    .do(ctx => expect(ctx.stdout).to.equal('hello world!\n'))
     .it('says hello world!')
 
   test
     .stdout()
     .do(() => cmd.run(['--name', 'jeff']))
-    .do(output => expect(output.stdout).to.equal('hello jeff!\n'))
+    .do(ctx => expect(ctx.stdout).to.equal('hello jeff!\n'))
     .it('says hello jeff!')
 })

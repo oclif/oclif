7c091f54e742baef3374dfeaa203a5f1136019e8
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-28 03:49:17 -0800

    chore: add git plugin back

--- release.config.js
+++ release.config.js
@@ -15,6 +15,7 @@ module.exports = {
     {path: './scripts/release_example'},
     '@semantic-release/changelog',
     '@semantic-release/npm',
+    '@semantic-release/git',
     '@semantic-release/github',
   ],
 }

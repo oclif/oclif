20b3cede5b35aca8068f1fef31fc8798892ee263
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-08 14:16:12 -0700

    fix: use npm pack in test

--- test/run.js
+++ test/run.js
@@ -55,7 +55,7 @@ module.exports = file => {
         sh.exec('node ./bin/run hello')
         sh.exec('node ./bin/run')
         sh.exec('node ./bin/run --help')
-        sh.exec('npm pack')
+        sh.exec('npm pack --unsafe-perm')
         break
       case 'single':
         build(cmd, name)
@@ -72,7 +72,7 @@ module.exports = file => {
         sh.exec('node ./bin/run hello')
         sh.exec('node ./bin/run help hello')
         sh.exec('node ./bin/run hello --help')
-        sh.exec('npm pack')
+        sh.exec('npm pack --unsafe-perm')
         break
       case 'command':
         build('plugin', name)
@@ -82,14 +82,14 @@ module.exports = file => {
         sh.exec('node ./bin/run foo:bar:baz')
         sh.exec('node ./bin/run help foo:bar:baz')
         sh.exec('node ./bin/run foo:bar:baz --help')
-        sh.exec('npm pack')
+        sh.exec('npm pack --unsafe-perm')
         break
       case 'hook':
         build('plugin', name)
         generate('hook myhook --defaults --force')
         sh.exec('yarn test')
         sh.exec('node ./bin/run hello')
-        sh.exec('npm pack')
+        sh.exec('npm pack --unsafe-perm')
         break
       }
     })

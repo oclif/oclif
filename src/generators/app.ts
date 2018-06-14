d49ad5fcaef159dbb1df727515c5de4651c8718b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-06-14 09:24:39 -0700

    fix: js setups

--- src/generators/app.ts
+++ src/generators/app.ts
@@ -269,7 +269,7 @@ class App extends Generator {
       this.pjson.scripts.prepack = this.pjson.scripts.prepare = nps.series(`${rmrf} lib`, 'tsc')
     }
     if (['plugin', 'multi'].includes(this.type)) {
-      this.pjson.scripts.prepack = nps.series(`${rmrf} lib`, 'tsc', 'oclif-dev manifest', 'oclif-dev readme', 'npm shrinkwrap')
+      this.pjson.scripts.prepack = nps.series(this.pjson.scripts.prepare, 'oclif-dev manifest', 'oclif-dev readme', 'npm shrinkwrap')
       this.pjson.scripts.postpack = `${rmf} oclif.manifest.json`
       this.pjson.scripts.version = nps.series('oclif-dev readme', 'git add README.md')
       this.pjson.files.push('/oclif.manifest.json')

67b433dc2e31c533e13f3f4c495c732dff8b8d5e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-08-16 17:35:45 -0700

    feat: typescript 3.x

--- src/commands/multi.ts
+++ src/commands/multi.ts
@@ -1,4 +1,4 @@
-import AppCommand from '../app_command'
+import AppCommand from '../app-command'
 
 export default class extends AppCommand {
   static description = 'generate a new multi-command CLI'

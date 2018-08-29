37520cc298ab4ad2c55febecf9f9860e99bc8b3b
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-08-29 08:49:47 -0700

    fix: set project path for single CLIs
    Fixes #161

--- templates/single/bin/run.ts
+++ templates/single/bin/run.ts
@@ -2,9 +2,12 @@
 
 const fs = require('fs')
 const path = require('path')
-const dev = fs.existsSync(path.join(__dirname, '../tsconfig.json'))
+const project = path.join(__dirname, '../tsconfig.json')
+const dev = fs.existsSync(project)
 
-if (dev) require('ts-node/register')
+if (dev) {
+  require('ts-node').register({project})
+}
 
 require(`../${dev ? 'src' : 'lib'}`).run()
 .catch(require('@oclif/errors/handle'))

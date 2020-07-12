d9716a51a30d288dc4eeb66eb49b406ab715b553
mohamed.taher <moslem.dev.2016@gmail.com>
2020-07-12 13:58:13 +0200

    new

--- src/commands/single.ts
+++
@@ -1,7 +0,0 @@
-import AppCommand from '../app-command'
-
-export default class extends AppCommand {
-  static description = 'generate a new single-command CLI'
-
-  type = 'single'
-}
---
+++ src/commands/tellmeajoke.ts
@@ -0,0 +1,21 @@
+  
+import {Command, flags} from '@oclif/command'
+import axios from "axios"
+
+export default class Tellmeajoke extends Command {
+  static description = 'describe the command here'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
+
+  async run() {
+    const {args, flags} = this.parse(Tellmeajoke)
+
+      let res = await axios.get(
+          `https://www.programmableweb.com/api/geek/geek-joke`
+          );
+
+    this.log(res.data)
+  }
+}

3a8644aa81a8fb634c5dfdeb95a9b703dcf0cf7e
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2021-02-05 12:54:05 -0800

    lib dir

--- lib/commands/upload/tarballs.d.ts
+++ lib/commands/upload/tarballs.d.ts
@@ -5,6 +5,7 @@ export default class UploadTarballs extends Command {
     static flags: {
         root: flags.IOptionFlag<string>;
         targets: flags.IOptionFlag<string>;
+        xz: import("@oclif/parser/lib/flags").IBooleanFlag<boolean>;
     };
     run(): Promise<void>;
 }

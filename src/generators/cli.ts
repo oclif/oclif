4d0b6976322ae9097bc119f192d2d591fb8d3008
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2022-01-10 13:35:45 +0330

    fix: fix deprecation warning for `fs.rmdirSync`

--- src/generators/cli.ts
+++ src/generators/cli.ts
@@ -72,7 +72,7 @@ class App extends Generator {
     this.log(yosay(`${msg} Version: ${version}`))
 
     execSync(`git clone https://github.com/oclif/hello-world.git ${path.resolve(this.name)}`)
-    fs.rmdirSync(`${path.resolve(this.name, '.git')}`, {recursive: true})
+    fs.rmSync(`${path.resolve(this.name, '.git')}`, {recursive: true, force: true})
 
     this.destinationRoot(path.resolve(this.name))
     process.chdir(this.destinationRoot())

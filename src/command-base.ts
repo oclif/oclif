09bf908e0b1b4283b376ca729f5b5d18d93a8b35
Mike Donnalley <mdonnalley@salesforce.com>
2022-07-27 09:53:40 -0600

    fix: await yeoman generators

--- src/command-base.ts
+++ src/command-base.ts
@@ -2,15 +2,14 @@ import {Command} from '@oclif/core'
 import {createEnv} from 'yeoman-environment'
 
 export default abstract class CommandBase extends Command {
-  protected generate(type: string, generatorOptions: Record<string, unknown> = {}): void {
+  protected async generate(type: string, generatorOptions: Record<string, unknown> = {}): Promise<void> {
     const env = createEnv()
 
     env.register(
       require.resolve(`./generators/${type}`),
       `oclif:${type}`,
     )
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    env.run(`oclif:${type}`, generatorOptions)
+
+    await env.run(`oclif:${type}`, generatorOptions)
   }
 }

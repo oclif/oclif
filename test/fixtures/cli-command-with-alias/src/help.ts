1c08b8331481e2c10e14a532d05f33a56d1e1876
Shazron Abdullah <36107+shazron@users.noreply.github.com>
2022-09-06 23:26:25 +0800

    feat: add `--aliases` flag for readme command (#925)
    * feat: add `--aliases` flag for readme command
    
    * fix: create test fixture to test --no-aliases flag
    
    * fix: eslint issues

---
+++ test/fixtures/cli-command-with-alias/src/help.ts
@@ -0,0 +1,7 @@
+import {Interfaces, Help} from '@oclif/core'
+
+export default class CustomHelp extends Help {
+  formatCommand(command: Interfaces.Command): string {
+    return `Custom help for ${command.id}`
+  }
+}

e42b94178c6d98e6437de24ded4460db4df1384d
Mike Donnalley <mdonnalley@salesforce.com>
2023-10-03 15:31:18 -0600

    test: use shelljs for cli integration test

--- test/integration/cli.test.ts
+++ test/integration/cli.test.ts
@@ -1,21 +1,9 @@
 import {expect} from 'chai'
-import {ExecOptions, exec as cpExec} from 'node:child_process'
 import {existsSync} from 'node:fs'
 import {mkdir, rm} from 'node:fs/promises'
 import {tmpdir} from 'node:os'
 import {join} from 'node:path'
-
-async function exec(command: string, opts: ExecOptions): Promise<{code: number; stderr: string; stdout: string}> {
-  return new Promise((resolve, reject) => {
-    cpExec(command, opts, (error, stdout, stderr) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve({code: 0, stderr, stdout})
-      }
-    })
-  })
-}
+import {exec} from 'shelljs'
 
 describe('Generated CLI Integration Tests', () => {
   const tmpDir = join(tmpdir(), 'generated-cli-integration-tests')
@@ -39,31 +27,31 @@ describe('Generated CLI Integration Tests', () => {
   })
 
   it('should generate a CLI', async () => {
-    const genResult = await exec(`${executable} generate ${cliName} --defaults`, {cwd: tmpDir})
+    const genResult = exec(`${executable} generate ${cliName} --defaults`, {cwd: tmpDir})
     expect(genResult.code).to.equal(0)
     expect(genResult.stdout).to.include(`Created ${cliName}`)
 
     setBins()
 
-    const result = await exec(`${cliBinRun} hello world`, {cwd: cliDir})
+    const result = exec(`${cliBinRun} hello world`, {cwd: cliDir})
     expect(result.code).to.equal(0)
     expect(result.stdout).to.equal('hello world! (./src/commands/hello/world.ts)\n')
   })
 
   it('should generate a new command', async () => {
-    const genResult = await exec(`${executable} generate command foo:bar:baz --force`, {cwd: cliDir})
+    const genResult = exec(`${executable} generate command foo:bar:baz --force`, {cwd: cliDir})
     expect(genResult.code).to.equal(0)
 
-    const result = await exec(`${cliBinDev} foo:bar:baz`, {cwd: cliDir})
+    const result = exec(`${cliBinDev} foo:bar:baz`, {cwd: cliDir})
     expect(result.code).to.equal(0)
     expect(result.stdout).to.include('hello world')
   })
 
   it('should generate a new hook', async () => {
-    const genResult = await exec(`${executable} generate hook init --event init --force`, {cwd: cliDir})
+    const genResult = exec(`${executable} generate hook init --event init --force`, {cwd: cliDir})
     expect(genResult.code).to.equal(0)
 
-    const result = await exec(`${cliBinDev} foo:bar:baz`, {cwd: cliDir})
+    const result = exec(`${cliBinDev} foo:bar:baz`, {cwd: cliDir})
     expect(result.code).to.equal(0)
     expect(result.stdout).to.include('example hook running foo:bar:baz\n')
   })

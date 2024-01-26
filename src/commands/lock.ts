85a29476468a7cd84481141c69cb847f185c3f9b
Mike Donnalley <mdonnalley@salesforce.com>
2024-01-26 13:07:16 -0700

    chore: add deprecation message

--- src/commands/lock.ts
+++ src/commands/lock.ts
@@ -24,6 +24,9 @@ export default class Lock extends Command {
     this.hidden = true
     this.state = 'deprecated'
     this.summary = 'Copy the yarn.lock to oclif.lock'
+    this.deprecationOptions = {
+      message: 'oclif.lock is deprecated and will be removed in @oclif/plugin-plugins v5.0.0 and above.',
+    }
     this.description = `Using oclif.lock allows your plugins dependencies to be locked to the version specified in the lock file during plugin install.
 Once the oclif.lock file is created you can include it your npm package by adding it to the files property of your package.json. We do not recommend committing the oclif.lock file to git.
 

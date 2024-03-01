c0eb1b4a2d42069ab1f872fe1e3a3a19565d61d4
Mike Donnalley <mdonnalley@salesforce.com>
2024-03-01 12:38:02 -0700

    fix: validate package name and bin

--- src/generators/cli.ts
+++ src/generators/cli.ts
@@ -2,6 +2,7 @@ import {Interfaces} from '@oclif/core'
 import {execSync} from 'node:child_process'
 import * as fs from 'node:fs'
 import * as path from 'node:path'
+import validatePkgName from 'validate-npm-package-name'
 import Generator from 'yeoman-generator'
 
 import {compact, isEmpty, uniq} from '../util'
@@ -159,12 +160,14 @@ export default class CLI extends Generator {
             message: 'npm package name',
             name: 'name',
             type: 'input',
+            validate: (d: string) => validatePkgName(d).validForNewPackages || 'Invalid package name',
           },
           {
             default: (answers: {name: string}) => answers.name,
             message: 'command bin name the CLI will export',
             name: 'bin',
             type: 'input',
+            validate: (d: string) => /^[\w-]+$/.test(d) || 'Invalid bin name',
           },
           {
             default: defaults.description,

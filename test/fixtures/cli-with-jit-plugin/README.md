7c2ee7cb860d870ec5cd78b423011344292daa21
Tristan Zander <tristan.zander@ncino.com>
2025-01-29 16:33:39 -0500

    feat: ensure jit plugins are downloaded via npm
    This prevents errors that occur due to tarballs needing
    to be downloaded with
    authentication. Using NPM's authentication schemes
    makes it simple and easy to include JIT plugins
    from within private organizations.

---
+++ test/fixtures/cli-with-jit-plugin/README.md
@@ -0,0 +1,17 @@
+# cli-with-jit-plugin
+
+This file is a test for running `oclif-dev readme` with aliases. If the --no-aliases flag
+is passed as a flag, no aliases should be in the README.
+
+<!-- toc -->
+<!-- tocstop -->
+
+# Usage
+
+<!-- usage -->
+<!-- usagestop -->
+
+# Commands
+
+<!-- commands -->
+<!-- commandsstop -->
--- test/unit/manifest.test.ts
+++ test/unit/manifest.test.ts
@@ -2,14 +2,19 @@ import {Interfaces} from '@oclif/core'
 import {runCommand} from '@oclif/test'
 import {expect} from 'chai'
 import {rm} from 'node:fs/promises'
+import path from 'node:path'
 
 describe('manifest', () => {
+  let cwd: string
+
   beforeEach(async () => {
+    cwd = process.cwd()
     await rm('oclif.manifest.json', {force: true})
   })
 
   afterEach(async () => {
     await rm('oclif.manifest.json', {force: true})
+    process.chdir(cwd)
   })
 
   it('should generate manifest', async () => {
@@ -18,5 +23,15 @@ describe('manifest', () => {
     expect(result?.commands.manifest).to.deep.include({
       description: 'Generates plugin manifest json (oclif.manifest.json).',
     })
+  })
+
+  it('should generate jit plugins', async () => {
+    process.chdir(path.join(__dirname, '../fixtures/cli-with-jit-plugin'))
+    const {error, stderr} = await runCommand<Interfaces.Manifest>('manifest', {
+      root: __dirname,
+    })
+
+    expect(stderr).contains('Generating JIT plugin manifests')
+    expect(error).to.be.undefined
   })
 })
